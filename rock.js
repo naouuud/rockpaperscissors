29012876a145fe5f5cab7377147ebee934be5d0d
Nour Aoude <naoude@gmail.com>
2022-05-01 00:43:10 +0300

    Functional game

--- rock.js
+++ rock.js
@@ -1,120 +1,52 @@
-console.log("Welcome to Rock, Paper, Scissors!");
-
 function computerPlay() {
-    let i = Math.floor(Math.random() * 3)
-    // console.log(i);
-    if (i === 0) {
-        return "rock"; 
-    } else if (i === 1) {
-        return "paper";
-    } else {
-        return "scissors";
+    let i = Math.floor(Math.random() * 3);
+    if (i === 0) return "rock"; 
+    else if (i === 1) return "paper";
+    else return "scissors";
     }
-}
 
-// fixed from recursive function to while loop
-function playerPrompt() {
-    let k = prompt("Select your weapon:", "");
-    // console.log(k);
-    if (k === null) {
-        return;
-    }
-    k = k.toLowerCase();
-    while (k !== 'rock' && k !== 'paper' && k !== 'scissors') {
-        alert("This is an invalid weapon.\n\nPlease choose rock, paper or scissors.");
-        k = prompt("Select your weapon:", "");
-        if (k === null) {
-            return;
+function playRound(compy, human) {
+    if (compy === 'rock') {
+        if (human === "rock") return;
+        else if (human === 'paper') return true;
+        else return false;
         }
-        k = k.toLowerCase();
+    if (compy === 'paper') {
+        if (human === "paper") return;
+        else if (human === 'scissors') return true;
+        else return false;
+        }
+    if (compy === 'scissors') {
+        if (human === "scissors") return;
+        else if (human === 'rock') return true;
+        else return false;
+        }
     }
-    console.log("The player has chosen " + k + ".");
-    return k;
-}
 
-function playRound(a, b) {
-//  if (b === 'a') {
-//      alert("And now you understand recursion.")
-//  }
-//  console.log('Player selection is ' + b);
-//  if (b === undefined) {
-//  return;
-//  }
-    console.log('The computer has chosen ' + a + '.');
-    if (a === 'rock') {
-        if (b === "rock") {
-//          alert("It's a tie!");
-            return;
-        } else if (b === 'paper') {
-//          alert("You win! Paper beats rock!");
-            return true;
-        } else {
-//          alert("You lose! Rock beats scissors!");
-            return false;
-        }
-    }
-    if (a === 'paper') {
-        if (b === "paper") {
-//          alert("It's a tie!");
-            return;
-        } else if (b === 'scissors') {
-//          alert("You win! Scissors beat paper!");
-            return true;
-        } else {
-//          alert("You lose! Paper beats rock!");
-            return false;
-        }
-    }
-    if (a === 'scissors') {
-        if (b === "scissors") {
-//          alert("It's a tie!");
-            return;
-        } else if (b === 'rock') {
-//          alert("You win! Rock beats scissors!");
-            return true;
-        } else {
-//          alert("You lose! Scissors beats paper!");
-            return false;
-        }
-    }
-}
-
-function game() {
-let computerScore = 0;
 let playerScore = 0;
-for (let i = 0; i < 5; i++) {
-let computerSelection = computerPlay();
-let playerSelection = playerPrompt();
-if (playerSelection === undefined) {
-    console.log("You have terminated the game.")
-    return;
-}
-let result = playRound(computerSelection, playerSelection);
-// console.log(result);
-if (result === true) {
-    playerScore++;
-    console.log("The player has won round " + (i + 1) + ".");
-}
-else if (result === false) {
-    computerScore++;
-    console.log("The computer has won round " + (i + 1) + ".");
-} 
-else {
-    console.log("Round " + (i + 1) + " was a tie.");
-}
-}
-if (playerScore > computerScore) {
-    console.log("Congratulations! You defeated the computer!");
-}
-else if (computerScore > playerScore) {
-    console.log("Tough luck, the computer won!");
-}
-else {
-    console.log("You tied with the computer. Try again next time.")
-}
+let computerScore = 0;
+let round = 1;
+
+function playGame(a, b) {
+    console.log('Round ' + round + ':');
+    const result = playRound(a, b);
+        console.log('The computer has chosen ' + a + '.');
+        console.log('The player has chosen ' + b + '.');
+        if (result === true) {
+            console.log('Player wins.');
+            playerScore++;
+        }
+        else if (result===false) {
+            console.log('Computer wins.');
+            computerScore++;
+        }
+        else console.log ('It\'s a tie');
+        console.log('Player score: ' + playerScore + '  Computer score: ' + computerScore);
+    if (playerScore === 5) console.log('THE PLAYER IS VICTORIOUS!');
+    if (computerScore === 5) console.log('THE COMPUTER IS VICTORIOUS!');
+    round ++;
 }
 
-game();
-
-// test
-// another test
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener('click', () => 
+                playGame(computerPlay(), button.className)));
