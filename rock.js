1eb2ea957736fa40ef7ae73eea0a85f69c92dd75
Nour Aoude <naoude@gmail.com>
2022-05-04 14:35:22 +0300

    Colors show round winner

--- rock.js
+++ rock.js
@@ -1,41 +1,30 @@
 function computerPlay() { //generates random computer weapon
     let i = Math.floor(Math.random() * 3);
     if (i === 0) {
-        changeColor('rock');
         return "rock";
     }
     else if (i === 1) {
-        changeColor('paper');
         return "paper";
     }
     else {
-        changeColor('scissors');
         return "scissors";
     }
 }
 
-const computerButtons = document.querySelectorAll('button.computer');
-
-function changeColor(rps) { //changes selected computer weapon color
-    computerButtons.forEach(button => button.style.backgroundColor = null);
-    const button = document.querySelector(`button.computer.${rps}`);
-    button.style.backgroundColor = 'orange';
-}
-
 function playRound(compy, human) { //compares computer and player selections
     if (compy === 'rock') {
-        if (human === "player rock") return;
-        else if (human === 'player paper') return true;
+        if (human === "rock") return;
+        else if (human === 'paper') return true;
         else return false;
         }
     if (compy === 'paper') {
-        if (human === "player paper") return;
-        else if (human === 'player scissors') return true;
+        if (human === "paper") return;
+        else if (human === 'scissors') return true;
         else return false;
         }
     if (compy === 'scissors') {
-        if (human === "player scissors") return;
-        else if (human === 'player rock') return true;
+        if (human === "scissors") return;
+        else if (human === 'rock') return true;
         else return false;
         }
     }
@@ -69,9 +58,23 @@ function playGame(a, b) {
     const result = playRound(a, b); //calls playRound to determine round winner
         if (result === true) {
             playerScore++;
+            const player = document.querySelector(`#${b}`);
+            const compy = document.querySelector(`.computer.${a}`);
+            player.style.backgroundColor = 'green';
+            compy.style.backgroundColor = 'red';
         }
         else if (result===false) {
             computerScore++;
+            const player = document.querySelector(`#${b}`);
+            const compy = document.querySelector(`.computer.${a}`);
+            player.style.backgroundColor = 'red';
+            compy.style.backgroundColor = 'green';
+        }
+        else {
+            const player = document.querySelector(`#${b}`);
+            const compy = document.querySelector(`.computer.${a}`);
+            player.style.backgroundColor = 'orange';
+            compy.style.backgroundColor = 'orange';
         }
     updateScore(); 
     updateRound();
@@ -106,11 +109,13 @@ function resetGame() {
     computerButtons.forEach(button => button.style.backgroundColor = null);
 }
 
+const computerButtons = document.querySelectorAll('button.computer');
 const playerButtons = document.querySelectorAll("button.player");
 playerButtons.forEach(button => button.addEventListener('click', (e) => startGame(e)));
 
 function startGame(e) {
+    computerButtons.forEach(button => button.style.backgroundColor = null);
     playerButtons.forEach(button => button.style.backgroundColor = null);
     e.srcElement.style.backgroundColor = 'blue';
-    playGame(computerPlay(), e.srcElement.className);
+    playGame(computerPlay(), e.srcElement.id);
 }
